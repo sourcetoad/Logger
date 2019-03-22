5e681dd47673ecc46b4d11fda9e3b5d7e6717467
Connor Tumbleson <connor@sourcetoad.com>
2019-03-22 17:04:14 -0400

    feat: add initial config file with configurations for user table

---
+++ src/config/activity-logger.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | User Table
+    |--------------------------------------------------------------------------
+    |
+    | The default user information used to create foreign keys in migrations.
+    |
+    */
+
+    'user' => [
+        'table'       => 'users',
+        'foreign_key' => 'id',
+    ],
+];
