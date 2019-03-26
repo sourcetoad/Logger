7f90cc8721ff82a65f46c09bdd07d82e4668a74c
Connor Tumbleson <connor@sourcetoad.com>
2019-03-26 17:36:12 -0400

    feat: methods for generating/mutating audit keys

--- src/Models/AuditKey.php
+++ src/Models/AuditKey.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 namespace Sourcetoad\Logger\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 /**
  * Class AuditKey
@@ -14,5 +15,49 @@ use Illuminate\Database\Eloquent\Model;
  */
 class AuditKey extends Model
 {
+    protected $fillable = [
+        'data',
+        'hash',
+    ];
+
     public $timestamps = false;
+
+    //--------------------------------------------------------------------------------------------------------------
+    // Mutators
+    //--------------------------------------------------------------------------------------------------------------
+
+    protected function setRouteAttribute($value)
+    {
+        if (is_array($value)) {
+            $flattenedKeys = array_keys(Arr::dot($value));
+            sort($flattenedKeys);
+            $value = json_encode($flattenedKeys);
+        }
+
+        $this->attributes['data'] = $value;
+        $this->attributes['hash'] = md5($value);
+    }
+
+    //--------------------------------------------------------------------------------------------------------------
+    // Functions
+    //--------------------------------------------------------------------------------------------------------------
+
+    public static function createOrFind(array $keys): AuditKey
+    {
+        $flattenedKeys = array_keys(Arr::dot($keys));
+        sort($flattenedKeys);
+
+        $jsonBlob = json_encode($flattenedKeys);
+        $jsonHash = md5($jsonBlob);
+
+        /** @var AuditKey $key */
+        $key = AuditKey::query()
+            ->firstOrCreate([
+                'hash' => $jsonHash
+            ], [
+                'data' => $jsonBlob
+            ]);
+
+        return $key;
+    }
 }
