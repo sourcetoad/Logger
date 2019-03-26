8a03cd1e5e1cd0091213ab83e3c48f2d7895ab3e
Connor Tumbleson <connor@sourcetoad.com>
2019-03-26 17:36:00 -0400

    feat: add functions for mutating/making audit routes

--- src/Models/AuditRoute.php
+++ src/Models/AuditRoute.php
@@ -14,5 +14,38 @@ use Illuminate\Database\Eloquent\Model;
  */
 class AuditRoute extends Model
 {
+    protected $fillable = [
+        'route',
+        'route_hash'
+    ];
+
     public $timestamps = false;
+
+    //--------------------------------------------------------------------------------------------------------------
+    // Mutators
+    //--------------------------------------------------------------------------------------------------------------
+
+    protected function setRouteAttribute($value)
+    {
+        $this->attributes['route'] = strtolower(trim($value));
+        $this->attributes['route_hash'] = md5($this->attributes['route']);
+    }
+
+    //--------------------------------------------------------------------------------------------------------------
+    // Functions
+    //--------------------------------------------------------------------------------------------------------------
+
+    public static function createOrFind(string $route): AuditRoute
+    {
+        $routeHash = md5(strtolower($route));
+
+        /** @var AuditRoute $route */
+        $route = AuditRoute::query()->firstOrCreate([
+            'route_hash' => $routeHash,
+        ], [
+            'route' => $route,
+        ]);
+
+        return $route;
+    }
 }
