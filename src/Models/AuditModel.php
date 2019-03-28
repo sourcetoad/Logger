5499ae6c376f064b69da11d045e1611121b38940
Connor Tumbleson <connor@sourcetoad.com>
2019-03-28 11:36:28 -0400

    feat: model for audit models

---
+++ src/Models/AuditModel.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types = 1);
+
+namespace Sourcetoad\Logger\Models;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+use Sourcetoad\Logger\Traits\Immutable;
+
+/**
+ * Class AuditModel
+ * @package Sourcetoad\Logger\Models
+ * @property int $id
+ * @property int $activity_id
+ * @property int $entity_type
+ * @property int $entity_id
+ * @property int|null $user_id
+ * @property-read User|null $user
+ */
+class AuditModel extends Model
+{
+    use Immutable;
+
+    protected $fillable = [
+        'activity_id',
+        'entity_type',
+        'entity_id',
+    ];
+
+    public $timestamps = false;
+
+    //--------------------------------------------------------------------------------------------------------------
+    // Mutators
+    //--------------------------------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------------------------------
+    // Functions
+    //--------------------------------------------------------------------------------------------------------------
+
+    public static function createOrFind(AuditActivity $activity, int $modelType, int $modelId): AuditModel
+    {
+        /** @var AuditModel $model */
+        $model = AuditModel::query()->create([
+            'activity_id' => $activity->id,
+            'entity_type' => $modelType,
+            'entity_id'   => $modelId,
+        ]);
+
+        return $model;
+    }
+
+    //--------------------------------------------------------------------------------------------------------------
+    // Relations
+    //--------------------------------------------------------------------------------------------------------------
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
