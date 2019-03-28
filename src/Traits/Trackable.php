0bc9b64e35eec6840d515cc496cb84c9ff246d41
Connor Tumbleson <connor@sourcetoad.com>
2019-03-28 15:23:34 -0400

    feat: pass off changed attributes (except timestamps) into logger

--- src/Traits/Trackable.php
+++ src/Traits/Trackable.php
@@ -15,11 +15,16 @@ trait Trackable
             resolve(Logger::class)->logRetrievedModel($model);
         });
 
-        // TODO - events for updating (to detect changed variables)
         static::updated(function (Model $model) {
             $dirtyAttributes = $model->getDirty();
 
-            // TODO get keys and pass along to our function
+            // don't care about dates
+            unset($dirtyAttributes['updated_at']);
+            unset($dirtyAttributes['created_at']);
+
+            $keys = array_keys($dirtyAttributes);
+
+            resolve(Logger::class)->logChangedModel($model, $keys);
         });
     }
 }
