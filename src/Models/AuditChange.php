89a436c085432c0af648817bcf24e16b82d5c8e8
Connor Tumbleson <connor@sourcetoad.com>
2019-07-05 09:38:06 -0400

    fix: add "processed" flag to change/model for audits

--- src/Models/AuditChange.php
+++ src/Models/AuditChange.php
@@ -17,6 +17,7 @@ use Sourcetoad\Logger\Traits\Immutable;
  * @property int $entity_id
  * @property int|null $user_id
  * @property int|null $key_id
+ * @property bool $processed
  * @property-read AuditKey $key
  * @property-read User|null $user
  * @property-read Model $entity
@@ -30,6 +31,7 @@ class AuditChange extends BaseModel
         'entity_type',
         'entity_id',
         'key_id',
+        'processed',
     ];
 
     public $timestamps = false;
