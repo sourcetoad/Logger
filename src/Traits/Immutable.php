ecd163c6ade9e4279cbd0e3148cc3d5ab9acb8b2
Connor Tumbleson <connor@sourcetoad.com>
2019-07-05 11:32:31 -0400

    fix: allow edits to processed/user_id

--- src/Traits/Immutable.php
+++ src/Traits/Immutable.php
@@ -13,16 +13,16 @@ trait Immutable
     {
         static::updating(function (Model $model) {
 
-            // We need to allow updates for the AuditModel (user_id), so we can scope in the user_id later on.
+            // We need to allow updates for the AuditModel (user_id/processed)
             if ($model instanceof AuditModel) {
-                if ($model->isDirty('user_id') && $model->isClean(['id', 'activity_id', 'entity_type', 'entity_id', 'processed'])) {
+                if ($model->isDirty(['user_id', 'processed']) && $model->isClean(['id', 'activity_id', 'entity_type', 'entity_id'])) {
                     return true;
                 }
             }
 
-            // We need to allow updates for the AuditChange (user_id), so we can scope in the user_id later on.
+            // We need to allow updates for the AuditChange (user_id/processed)
             if ($model instanceof AuditChange) {
-                if ($model->isDirty('user_id') && $model->isClean(['id', 'activity_id', 'entity_id', 'key_id', 'processed'])) {
+                if ($model->isDirty(['user_id', 'processed']) && $model->isClean(['id', 'activity_id', 'entity_id', 'key_id'])) {
                     return true;
                 }
             }
