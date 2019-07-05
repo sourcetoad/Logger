a91f1a72c4a7ceca54de9988a1df33c607130e88
Connor Tumbleson <connor@sourcetoad.com>
2019-07-05 11:18:04 -0400

    fix: support for updating "user_id" of audit-change

--- src/Traits/Immutable.php
+++ src/Traits/Immutable.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 namespace Sourcetoad\Logger\Traits;
 
 use Illuminate\Database\Eloquent\Model;
+use Sourcetoad\Logger\Models\AuditChange;
 use Sourcetoad\Logger\Models\AuditModel;
 
 trait Immutable
@@ -14,10 +15,18 @@ trait Immutable
 
             // We need to allow updates for the AuditModel (user_id), so we can scope in the user_id later on.
             if ($model instanceof AuditModel) {
-                if ($model->isDirty('user_id') && $model->isClean(['id', 'activity_id', 'entity_type', 'entity_id'])) {
+                if ($model->isDirty('user_id') && $model->isClean(['id', 'activity_id', 'entity_type', 'entity_id', 'processed'])) {
                     return true;
                 }
             }
+
+            // We need to allow updates for the AuditChange (user_id), so we can scope in the user_id later on.
+            if ($model instanceof AuditChange) {
+                if ($model->isDirty('user_id') && $model->isClean(['id', 'activity_id', 'entity_id', 'key_id', 'processed'])) {
+                    return true;
+                }
+            }
+
             return false;
         });
 
