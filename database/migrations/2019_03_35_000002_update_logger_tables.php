4904ae0817324dc27ca2b8c311f2a5cf9a762bbb
Connor Tumbleson <connor@sourcetoad.com>
2019-07-05 09:32:43 -0400

    feat: introduce "key_id" and "processed" to determine if processed

---
+++ database/migrations/2019_03_35_000002_update_logger_tables.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Sourcetoad\Logger\Models\AuditChange;
+use Sourcetoad\Logger\Models\AuditKey;
+
+class UpdateLoggerTables extends Migration
+{
+    public function up()
+    {
+        if (config('database.default') !== 'mysql') {
+            throw new \InvalidArgumentException("MySQL is the only supported driver for this package.");
+        }
+
+        Schema::table('audit_changes', function (Blueprint $table) {
+            $table->boolean('processed')->default(false);
+            $table->integer('key_id', false, true);
+
+            $table
+                ->foreign('key_id')
+                ->references('id')
+                ->on('audit_keys')
+                ->onDelete('RESTRICT');
+        });
+
+        Schema::table('audit_models', function (Blueprint $table) {
+            $table->boolean('processed')->default(false);
+        });
+
+        AuditChange::query()->chunkById(200, function ($changes) {
+            /** @var AuditChange $change */
+            foreach ($changes as $change) {
+                $keys = AuditKey::createOrFind($change->fields);
+                $change->key_id = $keys->id;
+                $change->saveOrFail();
+            }
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('audit_changes', function (Blueprint $table) {
+            $table->dropColumn('processed');
+        });
+
+        Schema::table('audit_models', function (Blueprint $table) {
+            $table->dropColumn('processed');
+        });
+    }
+}
