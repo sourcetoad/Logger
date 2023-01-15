4b9399f2bdc4b456cb4df1c18d4ce6d86f0f6aac
Connor Tumbleson <iBotPeaches@users.noreply.github.com>
2023-01-15 08:49:23 -0500

    Audit Key Deduper (#32)
    * feat: dedupe keys during processing
    
    * test: assertions for deduper
    
    * docs: slight doc changes
    
    * fix: properly handle known root keys + numerics
    
    * fix: support blocklist keys from inclusion
    
    * feat: migration to correct old audit keys

---
+++ src/Helpers/DataArrayParser.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types = 1);
+
+namespace Sourcetoad\Logger\Helpers;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+
+class DataArrayParser
+{
+    public static function dedupe(array $data): array
+    {
+        return collect(Arr::dot($data))
+            ->keys()
+            ->map(function (string $key) {
+                $removableIndexPairs = [
+                    'data',
+                    'entries',
+                ];
+
+                $purgedKey = preg_replace([
+                    '/[0-9]+/',
+                    '/(\.)+/',
+                ], [
+                    '',
+                    '.'
+                ], Str::remove($removableIndexPairs, $key));
+
+                return trim($purgedKey, '.');
+            })
+            ->filter()
+            ->unique()
+            ->sort()
+            ->values()
+            ->toArray();
+    }
+}
---
+++ tests/LoggerArrayParserTest.php
@@ -0,0 +1,151 @@
+<?php
+declare(strict_types = 1);
+
+namespace Sourcetoad\Logger\Test;
+
+use Sourcetoad\Logger\Helpers\DataArrayParser;
+
+class LoggerArrayParserTest extends TestCase
+{
+    /** @dataProvider dataProvider */
+    public function testDedupeParser(array $input, array $expected): void
+    {
+        $this->assertEquals($expected, DataArrayParser::dedupe($input));
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            'empty' => [
+                'input' => [],
+                'expected' => []
+            ],
+            'single key' => [
+                'input' => [
+                    'username' => 'Foo'
+                ],
+                'expected' => [
+                    'username'
+                ]
+            ],
+            'rejected key' => [
+                'input' => [
+                    'data' => 'foo'
+                ],
+                'expected' => []
+            ],
+            'multiple keys' => [
+                'input' => [
+                    'username' => 'Foo',
+                    'password' => 'Bar'
+                ],
+                'expected' => [
+                    'password',
+                    'username',
+                ]
+            ],
+            'sorted properties' => [
+                'input' => [
+                    'z' => 'value',
+                    'a' => 'value',
+                ],
+                'expected' => [
+                    'a',
+                    'z'
+                ]
+            ],
+            'duplicate properties' => [
+                'input' => [
+                    0 => [
+                        'first_name' => 'Name',
+                        'last_name' => 'Last',
+                    ],
+                    1 => [
+                        'first_name' => 'Name2',
+                        'last_name' => 'Last2'
+                    ],
+                    2 => [
+                        'first_name' => 'Name3',
+                        'last_name' => 'Last3'
+                    ]
+                ],
+                'expected' => [
+                    'first_name',
+                    'last_name'
+                ]
+            ],
+            'deeply nested property' => [
+                'input' => [
+                    [
+                        'data' => [
+                            0 => [
+                                'id' => 'Value',
+                                'person' => [
+                                    'updated_at' => 'Value'
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'expected' => [
+                    'id',
+                    'person.updated_at'
+                ]
+            ],
+            'deeply nested numeric property' => [
+                'input' => [
+                    [
+                        'data' => [
+                            0 => [
+                                'id' => 'Value',
+                                'persons' => [
+                                    0 => [
+                                        'updated_at' => 'Value'
+                                    ],
+                                    1 => [
+                                        'updated_at' => 'Value'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'expected' => [
+                    'id',
+                    'persons.updated_at'
+                ]
+            ],
+            'entries deeply nested w/ numerics and duplicates' => [
+                'input' => [
+                    'entries' => [
+                        0 => [
+                            'content' => [
+                                'response' => [
+                                    'data' => [
+                                        21 => [
+                                            'created_at' => 'Value'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ],
+                        1 => [
+                            'content' => [
+                                'response' => [
+                                    'data' => [
+                                        22 => [
+                                            'created_at' => 'Value'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'expected' => [
+                    'content.response.created_at'
+                ]
+            ]
+        ];
+    }
+}
