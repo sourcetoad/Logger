3c95c70a4858763effafce5c99dde9f879a0a0b5
nilsdelaguardia <nils.delaguardia@sourcetoad.com>
2024-10-28 14:11:13 -0400

    test: Update Testcase to use TestBench

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,7 @@ namespace Sourcetoad\Logger\Test;
 use Sourcetoad\Logger\LoggerFacade;
 use Sourcetoad\Logger\LoggerServiceProvider;
 
-class TestCase extends \PHPUnit\Framework\TestCase
+class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function getPackageProviders($app): array
     {
