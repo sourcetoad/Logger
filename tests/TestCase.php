79d3a56abbd4e1c9139f8176bf585fb0e746fc40
nilsdelaguardia <nils.delaguardia@sourcetoad.com>
2024-10-29 16:19:45 -0400

    test: Use use for TestBenchTestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,8 +5,9 @@ namespace Sourcetoad\Logger\Test;
 
 use Sourcetoad\Logger\LoggerFacade;
 use Sourcetoad\Logger\LoggerServiceProvider;
+use Orchestra\Testbench\TestCase as TestBenchTestCase;
 
-class TestCase extends \Orchestra\Testbench\TestCase
+class TestCase extends TestBenchTestCase
 {
     protected function getPackageProviders($app): array
     {
