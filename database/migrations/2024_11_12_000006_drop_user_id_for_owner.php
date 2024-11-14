745054f3811e3e091185af9ff49fdf74639c79fd
Rodrigo Bondoc <rodrigo.bondoc@sourcetoad.com>
2024-11-14 09:04:33 -0800

    update: make owner_id and owner_type nullable

--- database/migrations/2024_11_12_000006_drop_user_id_for_owner.php
+++ database/migrations/2024_11_12_000006_drop_user_id_for_owner.php
@@ -9,7 +9,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('audit_changes', function (Blueprint $table) {
-            $table->morphs('owner');
+            $table->mediumInteger('owner_type', false, true)->nullable();
+            $table->integer('owner_id', false, true)->nullable();
+
             $table->dropColumn('user_id');
         });
     }
@@ -17,11 +19,22 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('audit_changes', function (Blueprint $table) {
+            $table->addColumn(
+                config('activity-logger.user.foreign_key_type', 'bigInteger'),
+                'user_id',
+                [
+                    'autoIncrement' => false,
+                    'unsigned' => true,
+                ]
+            )
+                ->nullable();
+
             $table
                 ->foreign('user_id')
                 ->references(config('activity-logger.user.foreign_key', 'id'))
                 ->on(config('activity-logger.user.table', 'users'))
                 ->onDelete('RESTRICT');
+
             $table->dropColumn('owner_id');
             $table->dropColumn('owner_type');
         });
